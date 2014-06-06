9ee8fc7703ec426aec8a3f81cd44cf5457c507e2
Guozheng Ge <gzge@yahoo-inc.com>
2014-06-06 10:10:10 -0700

    create the new RottenTomatoes projec

---
+++ RottenTomatoes/RottenTomatoes/GGZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GGZAppDelegate.h
+//  RottenTomatoes
+//
+//  Created by Guozheng Ge on 6/6/14.
+//  Copyright (c) 2014 gzge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GGZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
